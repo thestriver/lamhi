a75f9d84bf7cb804736d2e962736db14bf2f5d8b
thestriver <wannabeamumin@gmail.com>
2022-10-04 14:38:50 -0600

    2.0 prep

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -21,7 +21,7 @@
               <div className="group relative col-span-12 flex h-full flex-col items-start justify-end overflow-hidden rounded-xl md:col-span-6 xl:col-span-4">
                 <div className="block h-96 w-full transform bg-cover bg-center transition duration-300 ease-in-out hover:scale-110" 
                 style={{backgroundImage: `url(${'https://cdn.devdojo.com/images/may2021/orange.jpg'})`}}> </div> 
-                {/* replace bove with a downloaded public asset e.g `url(./assets/download.png)` */}
+                {/* replace above with a downloaded public asset e.g `url(./assets/download.png)` */}
                 <div className="relative z-20 h-auto w-full border-t-0 border-yellow-200 bg-blue-400 py-8 px-7 text-white">
                   <div className="absolute top-0 -mt-3.5 inline-block rounded-full bg-white px-4 py-2 text-xs font-semibold uppercase text-blue-500">
                     No Hassles</div>
@@ -35,7 +35,7 @@
               <div className="group relative col-span-12 flex h-full flex-col items-start justify-end overflow-hidden rounded-xl sm:col-span-12 sm:flex-row xl:col-span-4 xl:flex-col">
                 <div className="block h-96 w-full transform bg-cover bg-center transition duration-300 ease-in-out hover:scale-110" 
                 style={{ backgroundImage: `url(${'https://cdn.devdojo.com/images/may2021/gbc.jpg'})`}}></div>
-                {/* replace bove with a downloaded public asset e.g `url(./assets/download.png)` */}
+                {/* replace above with a downloaded public asset e.g `url(./assets/download.png)` */}
                 <div className="relative z-20 flex h-auto w-full flex-col items-start justify-center border-t-0 border-yellow-200 bg-yellow-400 py-8 px-7 text-white sm:h-full xl:h-auto">
                   <div className="absolute top-0 -mt-3.5 inline-block rounded-full bg-white px-4 py-2 text-xs font-semibold uppercase text-yellow-400 sm:relative sm:mb-5 xl:absolute xl:mb-0">
                     Seamless</div>
@@ -49,7 +49,7 @@
               <div className="col-span-12 grid grid-cols-12 gap-7">
                 <div className="col-span-12 flex flex-col items-start overflow-hidden rounded-xl shadow-sm md:col-span-6 lg:col-span-4">
                   <div className="relative flex flex-col items-start rounded-b-2xl border border-t-0 border-gray-200 bg-white px-6 py-4">
-                    <div className="inline-block inline-block flex w-auto items-center rounded-full bg-indigo-400 px-3 py-1.5 text-xs font-medium uppercase leading-none text-white">
+                    <div className="inline-block flex w-auto items-center rounded-full bg-indigo-400 px-3 py-1.5 text-xs font-medium uppercase leading-none text-white">
                       <span>COMING SOON</span>
                     </div>
                     <h2 className="text-base font-bold sm:text-lg md:text-xl pt-2">
@@ -59,7 +59,7 @@
       
                 <div className="col-span-12 flex flex-col items-start overflow-hidden rounded-xl shadow-sm md:col-span-6 lg:col-span-4">
                   <div className="relative flex flex-col items-start rounded-b-2xl border border-t-0 border-gray-200 bg-white px-6 py-4">
-                    <div className="inline-block inline-block flex w-auto items-center rounded-full bg-red-400 px-3 py-1.5 text-xs font-medium uppercase leading-none text-white">
+                    <div className="inline-block flex w-auto items-center rounded-full bg-red-400 px-3 py-1.5 text-xs font-medium uppercase leading-none text-white">
                       <span>COMING SOON</span>
                     </div>
                     <h2 className="text-base font-bold sm:text-lg md:text-xl pt-2">
@@ -69,7 +69,7 @@
       
                 <div className="col-span-12 flex flex-col items-start overflow-hidden rounded-xl shadow-sm md:col-span-6 lg:col-span-4">
                 <div className="relative flex flex-col items-start rounded-b-2xl border border-t-0 border-gray-200 bg-white px-6 py-4">
-                    <div className="inline-block inline-block flex w-auto items-center rounded-full bg-purple-500 px-3 py-1.5 text-xs font-medium uppercase leading-none text-white">
+                    <div className="inline-block flex w-auto items-center rounded-full bg-purple-500 px-3 py-1.5 text-xs font-medium uppercase leading-none text-white">
                       <span>Coming Soon</span>
                     </div>
                     <h2 className="text-base font-bold sm:text-lg md:text-xl pt-2">
